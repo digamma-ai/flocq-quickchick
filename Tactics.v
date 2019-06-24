a54c77cf3991b83778f69836acfa09a082872ec0
yarik <yroslav541@gmail.com>
2019-06-24 21:08:25 +0300

    Complete proof for finite sub-generator

--- Tactics.v
+++
@@ -1,8 +0,0 @@
-(** [tuple_inversion] inverses an equality of tuple into equalities for
-    each component. *)
-Ltac tuple_inversion :=
-  match goal with
-    | [ H : (_, _, _, _) = (_, _, _, _) |- _ ] => invc H
-    | [ H : (_, _, _) = (_, _, _) |- _ ] => invc H
-    | [ H : (_, _) = (_, _) |- _ ] => invc H
-  end.
