b47706e456c33002f5481bb3781490ec2694736d
yarik <yroslav541@gmail.com>
2019-06-27 21:42:17 +0300

    Refactor example

--- Example.v
+++ Example.v
@@ -1,46 +1,24 @@
 Require Import FlocqQuickChick.Generators.
-Require Import ZArith PArith Bool.BoolEq List Bool.BoolEq.
 Require Import Flocq.IEEE754.Binary.
-Import ListNotations.
 From QuickChick Require Import QuickChick.
-Set Warnings "-extraction-opaque-accessed,-extraction".
 
-Theorem nan1 : nan_pl 24 1 = true.
-Proof. auto. Qed.
-
-Definition Float_maxnum {prec emax : Z} (a b : binary_float prec emax) :=
-  match (Bcompare prec emax a b) with
-    | (Some Eq) => Some a
-    | (Some Lt) => Some b
-    | (Some Gt) => Some a
-    | None => None
+Definition Float_maxnum {prec emax} (a b : binary_float prec emax) :=
+  match Bcompare prec emax a b with
+    | Some Lt => b
+    | _ => a
   end.
 
-Definition predicate {prec emax : Z} (a b : binary_float prec emax) :=
-  match Float_maxnum a b with
-    | (Some m) => (*m >= a && m >= b && ((m = a) || (m = b))*)
-      match Bcompare prec emax m a with
-        | (Some Eq) | (Some Gt) => 
-          match Bcompare prec emax m b with
-            | (Some Eq) | (Some Gt) => 
-              match Bcompare prec emax m a with
-                | (Some Eq) => true
-                | _ => 
-                  match Bcompare prec emax m b with
-                    | (Some Eq) => true
-                    | _ => false
-                  end
-              end
-            | _ => false
-          end
-        | _ => false
-      end
-    | None => false
-  end.
+Definition maxnum_correct {prec emax} (a b : binary_float prec emax) :=
+  let m := Float_maxnum a b in
+      match Bcompare prec emax m a, Bcompare prec emax m b with
+        | Some Eq, Some Gt | Some Eq, Some Eq => true
+        | Some Gt, Some Eq => true
+        | _, _ => false
+      end.
 
 Definition pair_fin32_gen : G (binary32 * binary32) := 
   bindGen (fing32) (fun b1 =>
     bindGen (fing32) (fun b2 =>
       returnGen (b1, b2))).
 
-QuickChick (forAll pair_fin32_gen (fun '(b1, b2) => predicate b1 b2)).
+QuickCheck (forAll pair_fin32_gen (fun '(b1, b2) => maxnum_correct b1 b2)).
